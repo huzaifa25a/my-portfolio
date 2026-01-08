98873185acbc9bf5bf90bd0ec91f200eab3fab6b
Huzaifa25a <pachisahuzaifa@gmail.com>
2026-01-08 18:48:22 +0400

    UI Enhancements

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,13 +37,7 @@ const About = () => {
             <h1 className='text-[24px] mb-4 text-center'>About Me</h1>
             <p className='text-justify'>I’m a full-stack developer with a Master’s in Computer Applications. I specialize in building web applications using the MERN stack — React, Node.js, MongoDB, and Express. I’m passionate about creating sleek, functional, and user-friendly interfaces while ensuring high performance. When I'm not coding, you’ll find me gaming or planning my next adventure.</p>
           </div> <br/>
-          {/* <div className='flex flex-col items-center'>
-            <h1>What I do?</h1>
-            <p>I build web apps using the MERN stack — React, Node.js, MongoDB, and Express. My focus is on creating user-friendly interfaces, writing clean code, and ensuring performance is top-notch.</p>
-          </div> <br/> */}
           <div className='flex flex-col items-center'>
-            {/* <h1 className='mb-2 font-medium text-[18px]'>Freelance Life</h1> */}
-            {/* <p className='text-justify'>I also freelance on Fiverr, where I’ve earned a Level 2 Seller badge. I enjoy working with clients around the world and delivering work that exceeds expectations.</p> <br/><br/> */}
             <div className='flex flex-row gap-[20px] items-center'>
               <a
                 href="/Huzaifa P - Resume.pdf"
@@ -52,7 +46,6 @@ const About = () => {
               >
                 Download CV
               </a>
-              {/* <a href='https://www.fiverr.com/huzaifa25' target='_blank' className='text-[#5a5a5a] hover:text-[#393939] transition-transform duration-200 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.2)] hover:scale-105 hover:drop-shadow-[0px_4px_4px_rgba(0,0,0,0.3) active:text-[#393939] active:scale-105 active:drop-shadow-[0px_4px_4px_rgba(0,0,0,0.3)]'>Checkout my gig on Fiverr</a> */}
             </div>
           </div>   
         </div>
